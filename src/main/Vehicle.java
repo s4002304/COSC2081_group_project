4bb52bd385becc62d3df2871684ed003e7458ddd
duong <duongqlee@gmail.com>
2023-09-19 22:45:25 +0700

    add vehicle and vehicle interface

--- src/main/Vehicle.java
+++ src/main/Vehicle.java
@@ -1,13 +1,82 @@
 package main;
 
-public abstract class Vehicle {
-    String id;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-    public Vehicle(String id) {
+public abstract class Vehicle implements VehicleInterface {
+    private String id;
+    private String name;
+    private Port currentPort;
+    private double currentFuel;
+    private double fuelCapacity;
+    private double carryingCapacity;
+    private double totalWeight;
+    private ArrayList<Class> containerType;
+    private HashMap<String, Container> currentContainers;
+
+    public Vehicle(String id, String name, double carryingCapacity, double currentFuel, double fuelCapacity) {
         this.id = id;
+        this.name = name;
+        this.carryingCapacity = carryingCapacity;
+        this.currentFuel = currentFuel;
+        this.fuelCapacity = fuelCapacity;
     }
 
-    public String getId() {
-        return this.id;
+    public String getName() {
+        return this.name;
+    }
+
+    public Port getCurrentPort() {
+        return this.currentPort;
+    }
+
+    public void setCurrentPort(Port port) {
+        this.currentPort = port;
+    }
+
+    public int getTotalNumberOfContainers() {
+        return this.currentContainers.size();
+    }
+
+    public int getTotalNumberOfContainersByTypes(Class containerType) {
+        int count = 0;
+        for (Map.Entry<String, Container> set : this.currentContainers.entrySet()) {
+            Container currentContainer = set.getValue();
+            if (containerType.isInstance(currentContainer)) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public void refilFuel() {
+        this.currentFuel = this.fuelCapacity;
+    }
+
+    public double getTotalWeight() {
+        return this.totalWeight;
+    }
+
+    public HashMap<String, Container> getAllContainers() {
+        return this.currentContainers;
+    }
+
+    public boolean isLoadable(HashMap<String, Container> containers) {
+        // TODO: Implement
+        return false;
+    }
+
+    public void loadContainer(Container container) {
+        // TODO: Implement
+    }
+
+    public void unloadContainer(Container container) {
+        // TODO: Implement
+    }
+
+    public boolean isTravelableToPort(Port port) {
+        // TODO: Implement
+        return false;
     }
 }
--- src/main/VehicleInterface.java
+++ src/main/VehicleInterface.java
@@ -1,5 +1,29 @@
 package main;
 
-public class VehicleInterface {
-    
+import java.util.HashMap;
+
+interface VehicleInterface {
+    String getName();
+
+    Port getCurrentPort();
+
+    void SetCurrentPort(Port port);
+
+    int getTotalNumberOfContainers();
+
+    int getTotalNumberOfContainersByTypes(Class containerType);
+
+    void refilFuel();
+
+    double getTotalWeight();
+
+    HashMap<String, Container> getAllContainers();
+
+    boolean isLoadable(HashMap<String, Container> containers);
+
+    void loadContainer(Container container);
+
+    void unloadContainer(Container container);
+
+    boolean isTravelableToPort(Port port);
 }

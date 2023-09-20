42d01bc126fb409ceb17d17d7aba34707dd37d73
duong <duongqlee@gmail.com>
2023-09-20 11:48:47 +0700

    add methods for traffic history

--- src/main/TrafficHistory.java
+++ src/main/TrafficHistory.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -13,6 +15,37 @@ public class TrafficHistory {
     private String portOfArrival;
     private String status;
 
-    
+    public TrafficHistory(Vehicle vehicle, Port departurePort, Port arrivalPort,
+            Date departureDate, Date arrivalDate, String status) {
+        this.vehicleId = vehicle.getId();
+        this.totalWeight = vehicle.getTotalWeight();
+        this.carriedContainers = vehicle.getAllContainers();
+        this.departureDate = departureDate;
+        this.arrivalDate = arrivalDate;
+        this.portOfDeparture = departurePort.getId();
+        this.portOfArrival = departurePort.getId();
+        this.status = status;
+    }
 
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String toString() {
+        DateFormat dateFormater = new SimpleDateFormat("yyyy/MM/dd");
+        String result = "";
+        result += "Port of departure: " + this.portOfDeparture + "\n";
+        result += "Date of departure: "
+                + dateFormater.format(this.departureDate) + '\n';
+        result += "Port of arrival: " + this.portOfArrival + '\n';
+        result += "Date of arrival: " + dateFormater.format(this.arrivalDate)
+                + '\n';
+        result += "Vehicle: " + this.vehicleId + '\n';
+        result += "Total weight: " + this.totalWeight + '\n';
+        return result;
+    }
 }

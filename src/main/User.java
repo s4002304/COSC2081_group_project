51cd6481629bbe9e2f527d623f70012e386163b0
duck <s3978210@rmit.edu.vn>
2023-09-21 22:05:58 +0700

    added user interface

--- src/main/User.java
+++ src/main/User.java
@@ -1,16 +1,56 @@
 package main;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User {
     private String username;
     private String password;
-    private String role;
-    public ArrayList<Port> authorizedPorts;
+    private UserRole role;
+    private HashMap<String, Port> assignedPorts; // Mapping of usernames to assigned ports
 
-    public User(String username, String password, String role) {
+    public User(String username, String password, UserRole role) {
         this.username = username;
         this.password = password;
         this.role = role;
+        this.assignedPorts = new HashMap<>();
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public Port getAssignedPort() {
+        return assignedPorts.get(username); // Retrieve the assigned port for the current user
+    }
+
+    public boolean canAccessPort(Port port) {
+        if (role == UserRole.SYSTEM_ADMIN) {
+            return true; // System admin can access all ports
+        } else if (role == UserRole.PORT_MANAGER && port != null) {
+            // Port manager can access their assigned port
+            Port assignedPort = assignedPorts.get(username);
+            return assignedPort != null && assignedPort.getId().equals(port.getId());
+        }
+        return false; // Default to denying access
+    }
+
+    // Assign a port to a port manager
+    public void assignPort(String username, Port port) {
+        if (role == UserRole.SYSTEM_ADMIN) {
+            assignedPorts.put(username, port);
+        }
+    }
+}
+
+enum UserRole {
+    SYSTEM_ADMIN,
+    PORT_MANAGER
 }

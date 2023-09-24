eb0281f05dfea96a439ccd00fbca998a7b3ee878
duong <duongqlee@gmail.com>
2023-09-24 16:20:05 +0700

    add create container

--- src/main/ReeferTruck.java
+++ src/main/ReeferTruck.java
@@ -3,6 +3,7 @@ package main;
 public class ReeferTruck extends Truck {
     public ReeferTruck(String id, String name, double carryingCapacity, double currentFuel, double fuelCapacity) {
         super(id, name, carryingCapacity, currentFuel, fuelCapacity);
+        this.containerType.add(Refrigerated.class);
     }
 
 }
--- src/main/TankerTruck.java
+++ src/main/TankerTruck.java
@@ -3,5 +3,6 @@ package main;
 public class TankerTruck extends Truck{
     public TankerTruck(String id, String name, double carryingCapacity, double currentFuel, double fuelCapacity) {
         super(id, name, carryingCapacity, currentFuel, fuelCapacity);
+        this.containerType.add(Liquid.class);
     }
 }
